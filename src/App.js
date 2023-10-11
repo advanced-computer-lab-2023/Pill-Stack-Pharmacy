76402918fed1c6c4173e2e4f345ec912553c59fc
Betty <106391376+Dareen234@users.noreply.github.com>
2023-10-11 19:10:45 +0300

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const MongoURI = process.env.MONGO_URI ;
 const {patientRegister} = require("./Routes/userController");
 const {createPharmacistReq,searchMedicine} = require("./Routes/pharmacistController");
-const {addAdmin,viewPatientDet, PatientDetailsResults,removeUser} = require("./Routes/adminController");
+const {addAdmin,viewPatientDet, PatientDetailsResults} = require("./Routes/adminController");
 
 //App variables
 const app = express();
@@ -18,15 +18,15 @@ app.use(express.static('uploads'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set("views", path.join(__dirname, "Views"));
+
 const admin = require("./Routers/adminRoute");
 const pharmacist = require("./Routers/pharmacistRoute");
 const patient = require("./Routers/patientRoute");
 
- 
-
-
 app.use(bodyParser.urlencoded({extended:false}));
+
 const port = process.env.PORT || "8000";
+
 
 // configurations
 // Mongo DB
@@ -50,41 +50,39 @@ app.use("/admin",admin)
 app.use("/patient",patient)
 
 
+//Renders the admin home page 
+ app.get("/admin_home", (req, res) => {
+   res.render('admin_home')
+   });
 
-app.get("/admin_home", (req, res) => {
-  res.render('admin_home')
-  });
-
+//render the user home page
   app.get("/register", (req, res) => {
   res.render('register')
     });
+
+//Adds the user to the database
+  app.post("/addUser",patientRegister);
+
+// pharmacist register request
+  app.route('/pharmacist_register')
+   .get((req, res) => { res.render('pharmacistRegister')})
+   .post(createPharmacistReq);
+
+ 
+
+  //patient home page 
   app.get("/patient", (req, res) => {
     res.render('patient')
     });
-  // app.get("/availableMedicines", (req, res) => {
-  //   res.render('availableMedicines')
-  //   });
+
+ //pharmacist home page 
   app.get("/pharmacist_home", (req, res) => {
     res.render('pharmacist_home')
     });
    
-app.post("/addUser",patientRegister);
-
-app.get("/PatientDetailsResults",PatientDetailsResults);
 
 
-
-// pharmacist register request
-app.route('/pharmacist_register')
-  .get((req, res) => { res.render('pharmacistRegister')})
-  .post(createPharmacistReq);
-
-  app.route('/removeUser')
-  .get((req, res) => { res.render('removeUser')})
-  .post(removeUser);
-
+//app.get("/PatientDetailsResults",PatientDetailsResults);
 
 
 app.use(express.json());
-
-
