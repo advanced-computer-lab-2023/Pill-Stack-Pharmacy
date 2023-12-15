4d4c6cf153055fd9dba193a4fddc1512677c5ae6
Betty <106391376+Dareen234@users.noreply.github.com>
2023-12-15 21:23:34 +0200

    backend

--- backend/src/Routes/userController.js
+++ backend/src/Routes/userController.js
@@ -138,6 +138,29 @@ const getFullInfo = async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 };
+const orders = async (req, res) => {
+  const userId = req.user._id;
+  
+  console.log("DAREEN")
+  try {
+      // Get past orders (status: Delivered or Cancelled)
+      const pastOrders = await orderModel.find({ userId, status: { $in: ['Delivered', 'Cancelled'] } })
+      //sorts the date in descending order
+          .sort({ date_added: -1 })
+          .exec();
+
+      // Get current orders (status: Processing)
+      const currentOrders = await orderModel.find({ userId, status: 'Processing' })
+      //sorts the date in descending order
+          .sort({ date_added: -1 })
+          .exec();
+
+      res.json({ pastOrders, currentOrders });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 
 const generateRoom = () => {
@@ -276,4 +299,4 @@ const getMedAndRelatedProducts=async(req,res)=>{
 }
 
 module.exports = {searchMedicinePat, filterMedicinesByMedicinalUse,getAddresses,addDeliveryAddress, orderDetails,getFullInfo,
-  generateRoom,joinChatRoomPatient,getDoctorUsername,sendMessage,getMedAndRelatedProducts};
+  generateRoom,joinChatRoomPatient,getDoctorUsername,sendMessage,getMedAndRelatedProducts,orders};
